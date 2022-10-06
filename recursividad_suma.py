26248e2698fda4a9ce51898f3a203471e203bf5f
Agirre8 <inagi30@gmail.com>
2022-10-06 13:59:15 +0200

    factorial recursivo

--- recursividad_suma.py
+++ recursividad_suma.py
@@ -5,3 +5,12 @@ def sumarec(numero):
         return numero + sumarec(numero-1)
 
 print(sumarec(10))
+
+def factorialrec(numero):
+    if numero==1:
+        return 1
+    else:
+        return numero * sumarec(numero-1)
+
+print(factorialrec(4))
+
